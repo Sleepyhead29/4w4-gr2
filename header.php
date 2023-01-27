c7e0887a57d5d9836be50dfc46e5c2296025cd84
Naz <nazeefsuleman@gmail.com>
2023-01-27 08:57:46 -0500

    s1c2 Debut de la creation de notre theme

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php  wp_head(); ?>
+<body>
+    <header>
+        <h1>4W4-Creation d'interface</h1>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Modele par defaut
+ * 
+ */
+
+?>
+<?php get_header(); ?>
+<h1>Bienvenue au cours 4W4</h1> 
+<?php get_footer();?>

03b629eae6e15779627e5d76f7c0c2e22c670c4e
Naz <nazeefsuleman@gmail.com>
2023-01-27 10:48:07 -0500

    s1c2 Boucle while have_posts()

--- index.php
+++ index.php
@@ -6,5 +6,16 @@
 
 ?>
 <?php get_header(); ?>
-<h1>Bienvenue au cours 4W4</h1> 
+<main>
+    <?php 
+    if(have_posts()):
+        while (have_posts()) : the_post();
+        the_title('<h1>', '</h1>'); 
+        the_content();?>
+        <hr>
+        <?php 
+           endwhile;
+        endif;  
+     ?>
+</main>
 <?php get_footer();?>

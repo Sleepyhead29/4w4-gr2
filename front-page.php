4d72a7967dab823bf58a660e9f6ab2d5b45cac00
Naz <nazeefsuleman@gmail.com>
2023-02-10 11:06:20 -0500

    s3c3 ajout dun conteneur pour les articles et modifications du style

--- front-page.php
+++ front-page.php
@@ -7,14 +7,17 @@
 ?>
 <?php get_header(); ?>
 <main>
-    <h3>front-page.php</h3>
     <?php 
     if (have_posts()): 
       while (have_posts()) : the_post(); ?>
+      <div class="article-solo">
       <h1>
           <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
       </h1>
-      
+      <?php //the_content(); // Affiche le contenu complet de l'article ?>
+      <?php the_excerpt(); // Affiche un résumé de l'article ?>
+      <?= wp_trim_words(get_the_excerpt(),10, "&#10148;") ?>
+      </div>
           <hr>
       <?php endwhile;
   endif;
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
-# Laboratoire 1
+# Exercise #1
 ## 4w4 - Conception d'interface et développement Web
 ### Auteur: Nazeef Suleman
 #### Description: 
-- Installaation local de Wordpress
+- Installa-ation local de Wordpress
 - Création d'un thème simple
 - Création d'un dépot git local
 - Création d'un dépot github

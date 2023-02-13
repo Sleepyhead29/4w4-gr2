29f16d86c39c1201def545793a0ce348b346941e
Naz <nazeefsuleman@gmail.com>
2023-02-13 14:02:28 -0500

    s4c1 organisation Saas pour afficher les les blocflex cree2ation d'un fichier sass pour l'affichage de nos articles markdown

--- front-page.php
+++ front-page.php
@@ -7,20 +7,22 @@
 ?>
 <?php get_header(); ?>
 <main>
+    <section class="blocflex">
     <?php 
     if (have_posts()): 
       while (have_posts()) : the_post(); ?>
-      <div class="article-solo">
-      <h1>
+      <article class="blocflex">
+      
+      <h2>
           <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
-      </h1>
+      </h2>
       <?php //the_content(); // Affiche le contenu complet de l'article ?>
       <?php the_excerpt(); // Affiche un résumé de l'article ?>
-      <?= wp_trim_words(get_the_excerpt(),10, "&#10148;") ?>
-      </div>
-          <hr>
+      <p><?= wp_trim_words(get_the_excerpt(),10, "&#10148;") ?></p>
+      </article>
       <?php endwhile;
-  endif;
+            endif;
      ?>
+     </section>
 </main>
 <?php get_footer();?>
